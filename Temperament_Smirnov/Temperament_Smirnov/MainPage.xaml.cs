13b873449206d148ff5d1bc3d2d583ca0a01b1fe
Opilane TTHK <opilane@tthk.ee>
2020-11-17 12:30:27 +0200

    Add project files.

---
+++ Temperament_Smirnov/Temperament_Smirnov/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Temperament_Smirnov
+{
+    public partial class MainPage : CarouselPage
+    {
+        public MainPage()
+        {
+            var AirophobiePage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Airophobie",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center
+                        },
+                        new Image
+                        {
+                            Source =
+                        }
+                    }
+                }
+            };
+            var acrophobiaPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "acrophobia",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center
+                        }
+                    }
+                }
+            };
+            var kinophobiaPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "kinophobia",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center
+                        }
+                    }
+                }
+            };
+            var ArachnophobiaPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Arachnophobia",
+                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                            HorizontalOptions = LayoutOptions.Center
+                        }
+                    }
+                }
+            };
+            Children.Add(AirophobiePage);
+            Children.Add(acrophobiaPage);
+            Children.Add(kinophobiaPage);
+            Children.Add(ArachnophobiaPage);
+        }
+    }
+}
