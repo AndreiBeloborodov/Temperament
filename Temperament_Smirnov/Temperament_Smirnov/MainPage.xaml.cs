35e5a635ca6e9046545ee624232a3a1d1255b193
opilane <opilane@HP-CZC6297B8H>
2020-11-23 19:09:59 +0200

    2

--- Temperament_Smirnov/Temperament_Smirnov/MainPage.xaml.cs
+++ Temperament_Smirnov/Temperament_Smirnov/MainPage.xaml.cs
@@ -10,10 +10,15 @@ namespace Temperament_Smirnov
 {
     public partial class MainPage : CarouselPage
     {
-        Label acr, air, arac, kino;
+        public object Btn3_Clicked { get; }
+
         public MainPage()
         {
-            var AirophobiePage = new ContentPage
+            ImageButton btn1;
+            ImageButton btn2;
+            ImageButton btn3;
+            ImageButton btn4;
+            var start = new ContentPage
             {
                 Content = new StackLayout
                 {
@@ -21,39 +26,21 @@ namespace Temperament_Smirnov
                     {
                         new Label
                         {
-                            Text = "Airophobie",
-                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                            HorizontalOptions = LayoutOptions.Center
-        },
-                        new Image
-                        {
-                            Source = "Airophobie.jpg",
-                            VerticalOptions= LayoutOptions.CenterAndExpand,
-                        }
-                    }
-                }
-            };
-            var acrophobiaPage = new ContentPage
-            {
-                Content = new StackLayout
-                {
-                    Children =
-                    {
-                        new Label
-                        {
-                            Text = "acrophobia",
-                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                            HorizontalOptions = LayoutOptions.Center
+                            Text = "Стартовая страница\n\nВиды темперамента",
+                            TextColor = Color.SkyBlue,
+                            BackgroundColor = Color.Gray,
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
                         },
-                        new Image
-                        {
-                            Source = "acrophobia.jpg",
-                            VerticalOptions=LayoutOptions.Center
-                        }
                     }
                 }
             };
-            var kinophobiaPage = new ContentPage
+            btn1 = new ImageButton
+            {
+                Source = "acrophobia.jpg"
+            };
+            btn1.Clicked += Btn1_Clicked;
+            var acrophobia = new ContentPage
             {
                 Content = new StackLayout
                 {
@@ -61,19 +48,22 @@ namespace Temperament_Smirnov
                     {
                         new Label
                         {
-                            Text = "kinophobia",
-                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                            HorizontalOptions = LayoutOptions.Center
+                            Text = "акрофобия",
+                            TextColor = Color.Green,
+                            BackgroundColor = Color.Gray,
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
                         },
-                        new Image
-                        {
-                            Source = "kinophobia.jpg",
-                            VerticalOptions=LayoutOptions.Center
-                        }
+                        btn1
                     }
                 }
             };
-            var ArachnophobiaPage = new ContentPage
+            btn2 = new ImageButton
+            {
+                Source = "Airophobie.jpg"
+            };
+            btn2.Clicked += Btn2_Clicked1;
+            var Airophobie = new ContentPage
             {
                 Content = new StackLayout
                 {
@@ -81,22 +71,87 @@ namespace Temperament_Smirnov
                     {
                         new Label
                         {
-                            Text = "Arachnophobia",
-                            FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                            HorizontalOptions = LayoutOptions.Center
+                            Text = "Аерофобия",
+                            TextColor = Color.Orange,
+                            BackgroundColor = Color.Gray,
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label))
                         },
-                        new Image
-                        {
-                            Source = "Arachnophobia.jpg",
-                            VerticalOptions=LayoutOptions.Center
-                        }
+                        btn2
                     }
                 }
             };
-            Children.Add(AirophobiePage);
-            Children.Add(acrophobiaPage);
-            Children.Add(kinophobiaPage);
-            Children.Add(ArachnophobiaPage);
-            }
+            btn3 = new ImageButton
+            {
+                Source = "Arachnophobia.jpg"
+            };
+            btn3.Clicked += Btn3_Clicked1;
+            var Arachnophobia = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Арахнофобия.jpg",
+                            TextColor = Color.Purple,
+                            BackgroundColor = Color.Gray,
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            FontSize = Device.GetNamedSize (NamedSize.Large, typeof(Label)),
+                        },
+                        btn3
+                    }
+                }
+            };
+            btn4 = new ImageButton
+            {
+                Source = "kinophobia.jpg"
+            };
+            btn4.Clicked += Btn4_Clicked1;
+            var kinophobia = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children =
+                    {
+                        new Label
+                        {
+                            Text = "Кинофобия",
+                            TextColor = Color.Chocolate,
+                            BackgroundColor = Color.Gray,
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+                        },
+                        btn4
+                    }
+                }
+            };
+            Children.Add(start);
+            Children.Add(acrophobia);
+            Children.Add(kinophobia);
+            Children.Add(Arachnophobia);
+            Children.Add(Airophobie);
+        }
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://ru.wikipedia.org/wiki/%D0%90%D0%BA%D1%80%D0%BE%D1%84%D0%BE%D0%B1%D0%B8%D1%8F", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void Btn4_Clicked1(object sender, EventArgs e)
+        {
+            await browser.OpenAsync("https://ru.wikipedia.org/wiki/%D0%9A%D0%B8%D0%BD%D0%BE%D1%84%D0%BE%D0%B1%D0%B8%D1%8F", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void Btn3_Clicked1(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://ru.wikipedia.org/wiki/%D0%90%D1%80%D0%B0%D1%85%D0%BD%D0%BE%D1%84%D0%BE%D0%B1%D0%B8%D1%8F", BrowserLaunchMode.SystemPreferred);
+        }
+
+        private void Btn2_Clicked1(object sender, EventArgs e)
+        {
+            await Browser.OpenAsync("https://ru.wikipedia.org/wiki/%D0%90%D1%8D%D1%80%D0%BE%D1%84%D0%BE%D0%B1%D0%B8%D1%8F", BrowserLaunchMode.SystemPreferred);
         }
     }
+}
